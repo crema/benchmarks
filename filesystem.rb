dede0dee251a4785c9fd5385ddec42a8819a7107
whonz <whonzmail@gmail.com>
2016-07-10 15:48:48 +0900

    fix

--- filesystem.rb
+++ filesystem.rb
@@ -36,7 +36,7 @@ class FilesystemBenchmark
   end
 
   def clear_cache
-    `sudo bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"`
+    `sudo 2>/dev/null  bash -c "sync; echo 3 > /proc/sys/vm/drop_caches"`
   end
 
   def traversal(path)
@@ -57,50 +57,39 @@ class FilesystemBenchmark
   end
 
   def file_benchmark
-    with_dest_dir do
-      puts ''
-      puts 'file rw'
-      results = Benchmark.bm(32) do |x|
+    puts ''
+    puts 'file rw'
+    results = Benchmark.bm(32) do |x|
+      total = 1024 * count
+      size = total
+      count = total / size
+
+      while size >= 1
         clear_cache
-        x.report('w 1g') do
-          write_file(File.join(dest,'tmp.1G'), 1024 * count)
-        end
-        clear_cache
-        x.report('w 1M * 1024') do
-          for i in (0..count) do
-            write_file(File.join(dest,"tmp.1M.#{i}"), 1024)
+        with_dest_dir do
+          x.report("w #{Filesize.new(size * 1024).pretty} * #{count}") do
+            for i in (0..count) do
+              write_file(File.join(dest,"tmp.#{size * 1024}.#{i}"), size)
+            end
+          end
+          clear_cache
+          x.report("r #{Filesize.new(size * 1024).pretty} * #{count}") do
+            for i in (0..count) do
+              read_file(File.join(dest,"tmp.#{size * 1024}.#{i}"))
+            end
           end
         end
-        clear_cache
-        x.report('w 100K * 10240') do
-          for i in (0..(count*10)) do
-            write_file(File.join(dest,"tmp.100K.#{i}"), 100)
-          end
-        end
-        clear_cache
-        x.report('r 1g') do
-          read_file(File.join(dest,'tmp.1G'))
-        end
-        clear_cache
-        x.report('r 1M * 1024') do
-          for i in (0..count) do
-            read_file(File.join(dest,"tmp.1M.#{i}"))
-          end
-        end
-        x.report('r 100k * 10240') do
-          for i in (0..(count*10)) do
-            read_file(File.join(dest,"tmp.100k.#{i}"))
-          end
-        end
+        size = size / 10
+        count = total / size
       end
     end
   end
 
   def dir_benchmark
-    with_dest_dir do
-      puts ''
-      puts 'dir'
-      Benchmark.bm(32) do |x|
+    puts ''
+    puts 'dir'
+    Benchmark.bm(32) do |x|
+      with_dest_dir do
         clear_cache
         x.report("create #{count} dirs") do
           (1..count).to_a.shuffle.each do |i|
@@ -116,12 +105,12 @@ class FilesystemBenchmark
   end
 
   def mix_benchmark
-    with_dest_dir do
-      puts ''
-      puts 'mix'
-      Benchmark.bm(32) do |x|
-        dirs = (1..count).to_a
-        read.times {dirs += (1..count).to_a}
+    puts ''
+    puts 'mix'
+    Benchmark.bm(32) do |x|
+      dirs = (1..count).to_a
+      read.times {dirs += (1..count).to_a}
+      with_dest_dir do
         clear_cache
         x.report("read(#{read}) write(1) 100K * #{count}") do
           dirs.each do |i|
